d02c075bc0905c69a83b4f90b1a8b3f0af210a17
Josemberg Ferreira <josembergff@gmail.com>
2021-08-26 17:55:26 -0300

    adjus url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public urlSubTitle = environment.hostWeb;
   public appPages = [
     { title: 'Home', url: '/dashboard', icon: 'home' },
     { title: 'User', url: '/user', icon: 'person' },
